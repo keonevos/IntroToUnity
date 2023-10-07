1f9b23af1b5b3233c84c98a2aa36401e9303d160
YopSolo <youpsolo@hotmail.com>
2023-10-07 19:18:06 +0200

    ajout de commentaires

--- Assets/ScriptableObjects/ItemData.cs
+++ Assets/ScriptableObjects/ItemData.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+// ScriptableObject est une classe qui permet de créer des "data assets"
 [CreateAssetMenu(fileName = "ItemData", menuName = "ScriptableObjects/ItemData", order = 1)]
 public class ItemData : ScriptableObject
 {
@@ -11,11 +12,12 @@ public class ItemData : ScriptableObject
         Legendary
     }
 
-    // Default values
+    // variables publiques avec leurs valeurs par défaut
     public int ItemID = -1;
-    public float ItemPrice = 0.0f;
-    public string ItemName = "Default ItemName";
+    public float ItemPrice = 0.0f; // ! penser à mettre un f à la fin pour les float
+    public string ItemName = "Default ItemName"; // ! penser à mettre des guillemets pour les strings
     public bool IsDestroyable = false;
-    public ERarity Rarity = ERarity.Common;
-    public Vector3 ItemPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    public ERarity Rarity = ERarity.Common; // ici on utilise l'enum défini plus haut pour définir la rareté de l'objet par défaut
+    public Vector3 ItemPosition = new Vector3(0.0f, 0.0f, 0.0f); // par defaut l'objet est placé en 0,0,0 c'est a dire au centre de la scene/du monde
+    public Sprite ItemSprite; // ici on utilise un sprite pour l'icone de l'objet
 }
